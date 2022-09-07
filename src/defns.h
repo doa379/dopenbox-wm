f3653af0db4ba8b9bf532e0da7b2960bea34c3ed
doa379 <doa379@gmail.com>
2022-09-07 10:19:13 +0100

    defns.h

---
+++ src/defns.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <utility>
+#include <palette.h>
+#include <xkb.h>
+
+namespace dobwm {
+  static constexpr auto MOD01             { MOD1_KEY };
+  static constexpr auto MOD04             { MOD4_KEY };
+  static constexpr auto MOD14             { MOD1_KEY | MOD4_KEY };
+  static constexpr auto MODS4             { SHIFT_KEY | MOD4_KEY };
+  static constexpr auto MODC4             { CTRL_KEY | MOD4_KEY };
+  static constexpr auto MODSC4            { SHIFT_KEY | CTRL_KEY | MOD4_KEY };
+  using Kb = std::pair<int, Key>;
+}
